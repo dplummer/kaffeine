e468ddf3a562cf5eeb8f5391ddfb9a59e9901c17
Donald Plummer <dplummer@avvo.com>
2017-10-11 08:33:10 -0700

    Debug logging in CI

--- config/test.exs
+++ config/test.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :kafka_impl, :impl, KafkaImpl.KafkaMock
 
-config :logger, :console, level: :error
+config :logger, :console, level: String.to_atom(System.get_env("LOGGER_LEVEL") || "error")
 
 config :kaffeine, consumer_wait_ms: 5
